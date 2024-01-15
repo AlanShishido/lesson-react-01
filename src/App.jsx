fc62337ed4ed704d4ccdbe2ba3f748db8488f594
Hideyuki Shishido <shishido.hideyuki@gmail.com>
2024-01-15 15:16:45 -0400

    Sidebar Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,27 @@ import { Post } from "./Post"
 import { Header } from "./components/Header"
 
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar"
 
 function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Alan Hideyuki Shishido"
-        content="lorien alguma coisa"
-      />
-      <Post
-        author="Aline Lopes Pascoal"
-        content="Lorien Revealed"
-      />
-      <Post/>
-      <Post/>
-      <Post/>
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Alan Hideyuki Shishido"
+            content="lorien alguma coisa"
+          />
+          <Post
+            author="Aline Lopes Pascoal"
+            content="Lorien Revealed"
+          />
+        </main>
+      </div>
     </>
     
   )
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,31 @@
+import { PencilLine } from 'phosphor-react'
+
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img
+                className={styles.cover}
+                src="https://images.unsplash.com/flagged/photo-1562599838-8cc871c241a5?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            />
+
+            <div className={styles.profile}>
+                <img 
+                    className={styles.avatar}
+                    src="https://avatars.githubusercontent.com/u/51389025?v=4"
+                />
+
+                <strong>Alan Shishido</strong>
+                <span>Electrical Engineer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
