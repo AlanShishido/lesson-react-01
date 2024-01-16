007c0ef70613c6637af2773343584c9bc2c09819
Hideyuki Shishido <shishido.hideyuki@gmail.com>
2024-01-15 22:40:16 -0400

    Comment structure

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment () {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/lpgoulart.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Leonardo Goulart</strong>
+                            <time title='11 de Maio as 8:13' dataTime="2024-05-11 08:13:38">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={20} />
+                        </button>
+
+                    </header>
+                    <p>Muito bom Devo, parabéns!!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+
+        </div>
+    )
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment'
 import styles from './Post.module.css'
 
 export function Post (props) {
@@ -27,7 +28,21 @@ export function Post (props) {
             </p>
 
             </div>
-        
+            <form className={styles.commentForm}>
+                <strong>Deixei seu feedback</strong>
+
+                <textarea placeholder='Deixe um comentário'></textarea>
+
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+            </form>
+
+            <div className={styles.commentList}>
+                <Comment />
+                <Comment />
+                <Comment />
+            </div>
         </ article>
     )
 }
