1b1c00cc7b64a118066fa69179f72e552f464974
Hideyuki Shishido <shishido.hideyuki@gmail.com>
2024-01-18 23:44:16 -0400

    React Engines

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,8 +5,14 @@ import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 
 import styles from './Post.module.css'
+import { useState } from 'react'
+
+
 
 export function Post ({author, content, publishedAt}) {  
+    const [comments, setComments] = useState([])
+    const [newCommentText, setNewCommentText] = useState('')
+
     const publishedDataFormated = format(
         publishedAt, 
         "d 'de' LLLL 'às' HH:mm'h'", { locale: ptBr })
@@ -15,6 +21,31 @@ export function Post ({author, content, publishedAt}) {
         addSuffix: true,
         locale: ptBr
     })
+
+    const isNewCommentEmpty = newCommentText.length === 0
+
+    function handleCreateNewComment(){
+        event.preventDefault()
+
+        setComments([...comments, newCommentText])
+        setNewCommentText('')
+    }
+
+    function handleNewCommentText(){
+        event.target.setCustomValidity('')
+        setNewCommentText(event.target.value)
+    }
+
+    function deleteComment(commentToDelete) {
+        const commentWithoutDeletedOne = comments.filter(comment => {
+            return comment !== commentToDelete
+        })
+        // console.log(comment)
+        setComments(commentWithoutDeletedOne)
+    }
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Este campo é obrigatório')
+    }
 
     return (
         <article className={styles.post}>
@@ -34,33 +65,41 @@ export function Post ({author, content, publishedAt}) {
 
             <div className={styles.content}>
                 {content.map( element => {
-                    console.log(element)
                     if (element.type === 'paragraph') {
                         return (
-                            <p>{element.content}</p>
+                            <p key={element.content}>{element.content}</p>
                         )
                     }
                     if (element.type === 'link'){
                         return (
-                            <p><a href='#'>{element.content}</a></p>
+                            <p key={element.content}><a href='#'>{element.content}</a></p>
                         )
                     }
                 })}
             </div>
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixei seu feedback</strong>
 
-                <textarea placeholder='Deixe um comentário'></textarea>
+                <textarea 
+                    name="comment" 
+                    placeholder='Deixe um comentário'
+                    value={newCommentText}
+                    onChange={handleNewCommentText}
+                    onInvalid={handleNewCommentInvalid}
+                    required
+                />
 
                 <footer>
-                    <button type='submit'>Publicar</button>
+                    <button type='submit' disabled={isNewCommentEmpty}>Publicar</button>
                 </footer>
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {comments.map( comment => {
+                    return (
+                        <Comment key={comment} content={comment} onDeleteComment={deleteComment}/>
+                    )
+                })}
             </div>
         </ article>
     )
